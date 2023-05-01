7c0686cac673b73e954e3cb2b75cc38ca560491d
Joel Eakle <joel.c.eakle@gmail.com>
2023-05-01 13:05:58 -0400

    32131

--- script.js
+++
@@ -1,7 +0,0 @@
-const gridContainer = document.getElementById("grid-container");
-
-    for (let i = 0; i < 256; i++) {
-        const square = document.createElement("div");
-        square.classList.add("square");
-        gridContainer.appendChild(square);
-    }
