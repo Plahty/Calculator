67b37b98f4c21ab8d3ac88137621206d654f5aa5
Joel Eakle <joel.c.eakle@gmail.com>
2023-05-03 13:56:55 -0400

    Etch a sketch complted0

--- script.js
+++ script.js
@@ -1,16 +1,48 @@
 const gridContainer = document.getElementById("grid-container");
 const resetButton = document.getElementById("reset-button");
+const whiteButton = document.getElementById("white-button");
+const blackButton = document.getElementById("black-button");
+const redButton = document.getElementById("red-button");
+const orangeButton = document.getElementById("orange-button");
+const yellowButton = document.getElementById("yellow-button");
+const greenButton = document.getElementById("green-button");
+const blueButton = document.getElementById("blue-button");
+const purpleButton = document.getElementById("purple-button");
+const rowsInput = document.getElementById("rows-input");
+const colsInput = document.getElementById("cols-input");
+const createGridButton = document.getElementById("create-grid-button");
 
-// Create the 16x16 grid
-for (let i = 0; i < 256; i++) {
-  const square = document.createElement("div");
-  square.classList.add("square");
-  gridContainer.appendChild(square);
+let currentColor = "black";
+let isMouseDown = false;
 
-  // Add a click event listener to each square
-  square.addEventListener("click", function() {
-    square.style.backgroundColor = "black";
-  });
+// Function to create the grid
+function createGrid(rows, cols) {
+  gridContainer.innerHTML = ""; // Clear the grid container
+
+  // Set the grid container styles
+  gridContainer.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+
+  // Create the squares
+  for (let i = 0; i < rows * cols; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    gridContainer.appendChild(square);
+
+    // Add event listeners to each square
+    square.addEventListener("mousedown", function() {
+      isMouseDown = true;
+      square.style.backgroundColor = currentColor;
+    });
+    square.addEventListener("mouseenter", function() {
+      if (isMouseDown) {
+        square.style.backgroundColor = currentColor;
+      }
+    });
+    square.addEventListener("mouseup", function() {
+      isMouseDown = false;
+    });
+  }
 }
 
 // Add event listener to reset button
@@ -20,3 +52,37 @@ resetButton.addEventListener("click", function() {
     square.style.backgroundColor = "white";
   });
 });
+
+// Add event listeners to color buttons
+whiteButton.addEventListener("click", function() {
+  currentColor = "white";
+});
+blackButton.addEventListener("click", function() {
+  currentColor = "black";
+});
+redButton.addEventListener("click", function() {
+  currentColor = "red";
+});
+orangeButton.addEventListener("click", function() {
+  currentColor = "orange";
+});
+yellowButton.addEventListener("click", function() {
+  currentColor = "yellow";
+});
+greenButton.addEventListener("click", function() {
+  currentColor = "green";
+});
+blueButton.addEventListener("click", function() {
+  currentColor = "blue";
+});
+purpleButton.addEventListener("click", function() {
+  currentColor = "purple";
+});
+
+// Add event listener to create grid button
+createGridButton.addEventListener("click", function() {
+  const rows = parseInt(rowsInput.value);
+  const cols = parseInt(colsInput.value);
+  createGrid(rows, cols);
+});
+
